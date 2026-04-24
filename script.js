a81d8c4d9046a7b234029ce1edca201090757353
Luz Mendoza Alcantara <luzmmalcantara@gmail.com>
2026-04-23 21:31:22 -0300

    implement js functionality

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const name = "Zara Darkbane";
+const characterClass = "Hechicera";
+let level = 7;
+let health = 100;
+let mana = 50;
+let attack = 25;
+let defense = 10;
+
 function calculateDamage(attack, defense) {
     const damage = attack - defense;
     if (damage < 0) {
@@ -17,5 +25,13 @@ const canCastSpell = (currentMana, spellCost, isStunned) => {
 };
 
 function getPresentation(name, characterClass, level) {
-    return `${name} — ${characterClass} (Nivel ${level})`;
+    return `${name} — ${characterClass} (Nivel: ${level})`;
 }
+
+console.log("Daño calculado:", calculateDamage(attack, defense));
+console.log("¿Esta vivo?:", isAlive(health));
+
+const spellCost = 20; 
+const isStunned = false;
+console.log("¿Puede lanzar hechizo?:", canCastSpell(mana, spellCost, isStunned));
+console.log("Presentacion:", getPresentation(name, characterClass, level));
